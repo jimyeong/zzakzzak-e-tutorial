720215d7e033de24dac7d8b1acd300335da8934d
velopert <public.velopert@gmail.com>
2018-08-25 04:07:59 +0900

    3-3. 비로그인 시 HeaderContainer 에서 로그인버튼 보여주기

--- src/containers/base/HeaderContainer.js
+++ src/containers/base/HeaderContainer.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import Header from 'components/base/Header';
+import Button from 'components/common/Button';
 
 class HeaderContainer extends Component {
   render() {
     if (!this.props.visible) return null; // visible 값이 false 일 때에는 숨기기
-    return <Header right="오른쪽" />;
+
+    const { user } = this.props;
+    return (
+      <Header
+        right={
+          user ? null : (
+            <Button to="/login" outline>
+              로그인
+            </Button>
+          )
+        }
+      />
+    );
   }
 }
 
-export default connect(({ base }) => ({ visible: base.visible }))(HeaderContainer);
+export default connect(({ base, user }) => ({ visible: base.header, user: user.user }))(HeaderContainer);
