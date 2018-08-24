23120125aea63d8bb1401cb6608e8f74f6139db3
velopert <public.velopert@gmail.com>
2018-08-25 02:42:36 +0900

    2-3. Route 컴포넌트 사용하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import AppTemplate from 'components/base/AppTemplate';
 import Header from 'components/base/Header';
+import { AuthPage, TweetsPage } from 'pages';
 
 const App = () => {
-  return <AppTemplate header={<Header right="오른쪽" />}>내용이 들어갈자리</AppTemplate>;
+  return (
+    <AppTemplate header={<Header right="오른쪽" />}>
+      {/* TweetsPage 는 3종류의 주소에서 사용됨*/}
+      <Route exact path="/" component={TweetsPage} />
+      <Route path="/users/:username" component={TweetsPage} />
+      <Route path="/tags/:tag" component={TweetsPage} />
+
+      <Route path="/:authType(login|register)" component={AuthPage} />
+    </AppTemplate>
+  );
 };
 
 export default App;
