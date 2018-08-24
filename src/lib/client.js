5a1d606f4887aa5035d74d6896797fdef00874ea
velopert <public.velopert@gmail.com>
2018-08-25 04:53:24 +0900

    3-8. API 호출 할 준비하기

---
+++ src/lib/client.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? '/'
+      : 'https://zzakzzak-e.vlpt.us/',
+  withCredentials: true, // 다른 도메인에 httpOnly 쿠키 사용 할 때 필수
+});
+
+// client.get('...') 이런식으로 사용하면 됨
+
+export default client;
