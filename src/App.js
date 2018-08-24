c5c11709520908d089bb1f86cc1f14b95e28840f
velopert <public.velopert@gmail.com>
2018-08-25 06:16:59 +0900

    4-6. 앱 첫 로딩시 현재 로그인정보 확인

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import AppTemplate from 'components/base/AppTemplate';
 import { AuthPage, TweetsPage } from 'pages';
 import HeaderContainer from 'containers/base/HeaderContainer';
+import Core from 'containers/base/Core';
 
 const App = () => {
   return (
@@ -11,8 +12,8 @@ const App = () => {
       <Route exact path="/" component={TweetsPage} />
       <Route path="/users/:username" component={TweetsPage} />
       <Route path="/tags/:tag" component={TweetsPage} />
-
       <Route path="/:authType(login|register)" component={AuthPage} />
+      <Core />
     </AppTemplate>
   );
 };
