1fd4919a4425ffa10ac550b9a5b78f6bbfe75c2d
velopert <public.velopert@gmail.com>
2018-08-25 04:26:51 +0900

    3-6. auth 리덕스 모듈 만들기

---
+++ src/store/modules/auth.js
@@ -0,0 +1,40 @@
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+
+const INITIALIZE = 'auth/INITIALIZE'; // 초기화
+const CHANGE_INPUT = 'auth/CHANGE_INPUT'; // 인풋 값 변경
+const SET_ERROR = 'auth/SET_ERROR'; // 에러 설정
+
+export const initialize = createAction(INITIALIZE);
+export const changeInput = createAction(CHANGE_INPUT, ({ field, value }) => ({
+  field,
+  value,
+}));
+
+const initialState = {
+  fields: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  error: null,
+};
+
+const reducer = handleActions(
+  {
+    [INITIALIZE]: state => initialState,
+    [CHANGE_INPUT]: (state, { payload: { field, value } }) => {
+      return produce(state, draft => {
+        draft.fields[field] = value;
+      });
+    },
+    [SET_ERROR]: (state, { payload: error }) => {
+      return produce(state, draft => {
+        draft.error = error;
+      });
+    },
+  },
+  initialState
+);
+
+export default reducer;
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { penderReducer } from 'redux-pender';
 import base from './base';
 import user from './user';
+import auth from './auth';
 
 export default combineReducers({
   base,
   user,
+  auth,
   pender: penderReducer,
 });
