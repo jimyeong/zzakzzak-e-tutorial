b7e365a3f75b104d69b0c495e75c0f2b607f7a6a
velopert <public.velopert@gmail.com>
2018-08-25 08:04:56 +0900

    6-5. TweetItemListContainer 컨테이너 컴포넌트 만들기

---
+++ src/containers/tweets/TweetItemListContainer.js
@@ -0,0 +1,71 @@
+import React, { Component, Fragment } from 'react';
+import TweetItemList from 'components/tweets/TweetItemList';
+import * as tweetActions from 'store/modules/tweets';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { withRouter } from 'react-router-dom';
+
+class TweetItemListContainer extends Component {
+  initialize = async () => {
+    // 현재 선택된 태그, 유저명에 따라 초기 요청을 넣어줍니다.
+    // didMount 와 didUpdate 에서 호출됩니다.
+    const {
+      TweetActions,
+      match: { params },
+    } = this.props;
+    const { tag, username } = params;
+    try {
+      await TweetActions.getInitial({
+        username,
+        tag,
+      });
+    } catch (e) {}
+  };
+
+  componentDidMount() {
+    this.initialize();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // 특정 조건이 바뀌었는지 검사를 합니다
+    if (
+      this.props.match.params.username !== prevProps.match.params.username ||
+      this.props.match.params.tag !== prevProps.match.params.tag
+    ) {
+      // 그래서 유저명이나 태그 바뀌면 재요청
+      // 리액트 라우터에서 주소에 따라 다른걸 불러와야하면 이렇게
+      // cdu 에서 작업 하시면 됩니다.
+      this.initialize();
+    }
+  }
+
+  render() {
+    const { list, username } = this.props;
+
+    return (
+      <Fragment>
+        <TweetItemList
+          tweets={list}
+          onRemove={this.handleOpenRemoveModal}
+          currentUser={username}
+        />
+      </Fragment>
+    );
+  }
+}
+
+const enhance = compose(
+  withRouter,
+  connect(
+    ({ tweets, user }) => ({
+      list: tweets.list,
+      end: tweets.end,
+      username: user.user && user.user.username,
+    }),
+    dispatch => ({
+      TweetActions: bindActionCreators(tweetActions, dispatch),
+    })
+  )
+);
+export default enhance(TweetItemListContainer);
--- src/pages/TweetsPage.js
+++ src/pages/TweetsPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TweetsTemplate from 'components/tweets/TweetsTemplate';
 import TweetWriteContainer from 'containers/tweets/TweetWriteContainer';
+import TweetItemListContainer from 'containers/tweets/TweetItemListContainer';
 
 const TweetsPage = ({ match }) => {
   return (
     <TweetsTemplate>
       {match.path === '/' && <TweetWriteContainer />}
+      <TweetItemListContainer />
     </TweetsTemplate>
   );
 };
