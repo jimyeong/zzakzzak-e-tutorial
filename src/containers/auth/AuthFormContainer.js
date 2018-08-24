20824dbe38a65ce5be50490e43ef785f86cc0164
velopert <public.velopert@gmail.com>
2018-08-25 06:03:03 +0900

    4-4. handleRegister, handleLogin 함수 구현하기

--- src/containers/auth/AuthFormContainer.js
+++ src/containers/auth/AuthFormContainer.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { compose, bindActionCreators } from 'redux';
 import AuthForm from 'components/auth/AuthForm';
 import * as authActions from 'store/modules/auth';
+import * as userActions from 'store/modules/user';
+import { saveUser } from 'lib/common';
 
 class AuthFormContainer extends Component {
   handleChange = e => {
@@ -26,11 +28,67 @@ class AuthFormContainer extends Component {
   };
 
   handleRegister = async () => {
-    // 구현 예정
+    // 가입을 합니다.
+    const { fields, AuthActions, UserActions } = this.props;
+    const { username, password, passwordConfirm } = fields;
+
+    // 우선 다양한 규칙을 검사합니다.
+    if (password !== passwordConfirm) {
+      AuthActions.setError('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
+    if (!/^[a-z-0-9-_]{3,15}$/.test(username)) {
+      AuthActions.setError('계정명은 3~15자의 영소문자/숫자여야합니다');
+      return;
+    }
+
+    if (password.length < 6) {
+      AuthActions.setError('비밀번호가 너무 짧습니다.');
+      return;
+    }
+
+    // 다 통과했으면 가입요청을 시작합니다
+    try {
+      await AuthActions.register({
+        username,
+        password,
+      });
+      // 이 시점에서는 쿠키 설정이 완료된 상태입니다.
+      // checkAuth 를 통하여 현재 계정 정보를 가져옵니다.
+      await UserActions.checkAuth();
+      // 계정 정보를 로컬 스토리지에 저장합니다.
+      // 여기서 [this].props.user 를 해주는것이 중요합니다.
+      // 꼭, this 에서 불러와야 업데이트되고 난 후의 데이터를 읽을 수 있습니다.
+      // 만약에 사전에 비구조화 할당 하신다면 이전의 데이터를 가르키게 됩니다.
+      saveUser(this.props.user);
+      // 모두 끝나고 홈 경로로 이동합니다.
+      this.props.history.push('/');
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   handleLogin = async () => {
-    // 구현 예정
+    // 로그인을 합니다. 흐름은 회원가입과 매우 비슷합니다.
+    const { fields, AuthActions, UserActions } = this.props;
+    const { username, password } = fields;
+    try {
+      if (!username) {
+        AuthActions.setError('아이디를 입력하세요.');
+        return;
+      }
+      if (!password) {
+        AuthActions.setError('비밀번호를 입력하세요.');
+        return;
+      }
+      await AuthActions.login({ username, password });
+      await UserActions.checkAuth();
+      saveUser(this.props.user);
+      this.props.history.push('/');
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   componentDidMount() {
@@ -67,9 +125,14 @@ class AuthFormContainer extends Component {
 const enhance = compose(
   withRouter,
   connect(
-    ({ auth }) => ({ fields: auth.fields, error: auth.error }),
+    ({ auth, user }) => ({
+      fields: auth.fields,
+      error: auth.error,
+      user: user.user,
+    }),
     dispatch => ({
       AuthActions: bindActionCreators(authActions, dispatch),
+      UserActions: bindActionCreators(userActions, dispatch),
     })
   )
 );
