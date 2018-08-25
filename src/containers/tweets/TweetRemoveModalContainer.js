5760d07b1187fcb29643d03c4448931ac445c855
velopert <public.velopert@gmail.com>
2018-08-25 09:28:16 +0900

    7-3. TweetRemoveModalContainer 컴포넌트 만들기

---
+++ src/containers/tweets/TweetRemoveModalContainer.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import TweetRemoveModal from 'components/tweets/TweetRemoveModal';
+import { connect } from 'react-redux';
+import { closeRemoveModal, remove } from 'store/modules/tweets';
+
+class TweetRemoveModalContainer extends Component {
+  handleClose = () => {
+    const { closeRemoveModal } = this.props;
+    closeRemoveModal();
+  };
+  handleRemoveConfirm = async pass => {
+    const { remove, closeRemoveModal } = this.props;
+    try {
+      await remove({
+        id: this.props.id,
+        pass,
+      });
+      closeRemoveModal();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  render() {
+    const { open, error, needPass } = this.props;
+    if (!open) return null;
+    return (
+      <TweetRemoveModal
+        onClose={this.handleClose}
+        onRemoveConfirm={this.handleRemoveConfirm}
+        error={error}
+        needPass={needPass}
+      />
+    );
+  }
+}
+
+export default connect(
+  ({ tweets }) => ({
+    open: tweets.removeModal.open,
+    error: tweets.removeModal.error,
+    id: tweets.removeModal.id,
+    needPass: tweets.removeModal.needPass,
+  }),
+  {
+    closeRemoveModal,
+    remove,
+  }
+)(TweetRemoveModalContainer);
--- src/pages/TweetsPage.js
+++ src/pages/TweetsPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import TweetsTemplate from 'components/tweets/TweetsTemplate';
 import TweetWriteContainer from 'containers/tweets/TweetWriteContainer';
 import TweetItemListContainer from 'containers/tweets/TweetItemListContainer';
+import TweetRemoveModalContainer from 'containers/tweets/TweetRemoveModalContainer';
 
 const TweetsPage = ({ match }) => {
   return (
     <TweetsTemplate>
       {match.path === '/' && <TweetWriteContainer />}
       <TweetItemListContainer />
+      <TweetRemoveModalContainer />
     </TweetsTemplate>
   );
 };
