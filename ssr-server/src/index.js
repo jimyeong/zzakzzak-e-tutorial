26bdb30e207b685571050863342668b70c3845f8
velopert <public.velopert@gmail.com>
2018-09-01 03:10:01 +0900

    9-3. 모든 경로에서 index.html 띄워주기

---
+++ ssr-server/src/index.js
@@ -0,0 +1,41 @@
+require('dotenv').load(); // .env 파일에서 환경변수 불러옵니다.
+const Koa = require('koa');
+const Router = require('koa-router');
+const koaStatic = require('koa-static');
+const path = require('path');
+const fs = require('fs'); // 파일을 읽어오는 모듈
+const app = new Koa();
+
+const port = process.env.PORT || 5000;
+
+// 테스트용 라우터 생성
+const router = new Router();
+router.get('/api/ping', ctx => {
+  ctx.body = {
+    message: 'pong',
+  };
+});
+
+// 라우터를 먼저 적용하고
+app.use(router.routes())
+app.use(router.allowedMethods());
+
+// 그다음에 정적 파일 제공
+const publicPath = path.join(__dirname, '../../build');
+app.use(koaStatic(publicPath));
+
+const indexPath = path.join(publicPath, 'index.html');
+const indexHtml = fs.readFileSync(indexPath);
+
+// fallback 함수
+app.use(ctx => {
+  // 이전 미들웨어에서 처리 할 수 없는 경우에만 이 fallback 이 실행됨
+  if (ctx.status !== 404 && !ctx.body) return;
+  ctx.status = 200;
+  ctx.set('Content-Type', 'text/html; charset=utf-8');
+  ctx.body = indexHtml;
+});
+
+app.listen(port, () => {
+  console.log('Server is listening to port ' + port);
+})
